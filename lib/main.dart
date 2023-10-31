5217c294c225084924d1a14f3a2e1078aaee00f6
maogpt <kelikejitech@gmail.com>
2023-10-31 17:17:24 +0800

    Handling gestures

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,31 @@ class TutorialHome extends StatelessWidget {
   }
 }
 
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
 // void main() {
 //   runApp(
 //     const MaterialApp(
@@ -109,8 +134,11 @@ class TutorialHome extends StatelessWidget {
 void main() {
   runApp(
     const MaterialApp(
-      title: 'Flutter Tutorial',
-      home: TutorialHome(),
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
+      ),
     ),
   );
 }
